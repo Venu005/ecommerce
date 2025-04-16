88ff3dd89229357af239835b0156a37c5df96c96
Venu005 <venusaiyalamanchili@gmail.com>
2025-04-16 14:45:15 +0530

    chore : layout complete

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Button variant={"elevated"}>hii</Button>
+    </div>
+  );
+}
