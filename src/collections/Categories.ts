185cc5af7909eb8ecc7e3d51795209172993d4a0
Venu005 <venusaiyalamanchili@gmail.com>
2025-04-17 22:18:56 +0530

    chore : add payload and Content schema

---
+++ src/collections/Categories.ts
@@ -0,0 +1,37 @@
+import type { CollectionConfig } from "payload";
+
+export const Categories: CollectionConfig = {
+  slug: "categories",
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "slug",
+      type: "text",
+      required: true,
+      unique: true,
+      index: true, // querying faster
+    },
+    {
+      name: "color",
+      type: "text",
+    },
+    {
+      // relation slef join
+      name: "parent",
+      type: "relationship",
+      relationTo: "categories",
+      hasMany: false,
+    },
+    {
+      name: "subcategories",
+      type: "join",
+      collection: "categories",
+      on: "parent",
+      hasMany: true,
+    },
+  ],
+};
